5c60a947e022ec9e90492007e9886bf0fe531e83
Jason <Git21019581>
2020-12-28 03:31:29 +0800

    回归本源

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import ElementUi from 'element-ui'
 
 //.是代表文件所在层，@在/build/webpack.base.conf.js中配置的
 import main from '../view/main'
 import test from '../view/test'
 
 Vue.use(VueRouter)
+Vue.use(ElementUi)
 
 export default new VueRouter({
   routes: [
---
+++ src/view/user/login.vue
@@ -0,0 +1,13 @@
+<template>
+<h2>smg</h2>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style scoped>
+
+</style>
